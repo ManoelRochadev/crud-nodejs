dfae930b0a705f53b08c7a17cf70e0ef04f4be9b
Manoel Rocha <manoelmelo367@gmail.com>
2022-06-01 01:05:37 +0000

    add cors

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ import { routes } from './routes'
 
 const app = express()
 
+app.use(cors())
+
 app.use(express.json())
 
 app.use(routes)
@@ -24,3 +26,7 @@ mongoose
 app.listen(process.env.PORT || 3333, () => {
   console.log('Server started')
 })
+function cors(): any {
+  throw new Error('Function not implemented.')
+}
+
